1f7476b8aadd512fa46d43a20ccd5022a2dc9617
Yasin Ünal <yasin.unal@bil.omu.edu.tr>
2024-12-26 13:34:10 +0300

    update login

--- ESOGU_WIFI_login/wifi_login.py
+++ ESOGU_WIFI_login/wifi_login.py
@@ -1,10 +1,42 @@
 import json
+import os
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-import os
-import time
+from pywifi import PyWiFi, const, Profile
+
+# Wi-Fi ağına bağlanma fonksiyonu
+def connect_to_wifi(ssid, password):
+    wifi = PyWiFi()
+    iface = wifi.interfaces()[0]  # İlk Wi-Fi adaptörünü al
+    iface.disconnect()
+    time.sleep(1)  # Bağlantı kesme süresi
+    
+    # Zaten bağlı mı kontrol et
+    if iface.status() == const.IFACE_CONNECTED:
+        print("Zaten bir ağa bağlısınız.")
+        return True
+
+    profile = Profile()
+    profile.ssid = ssid
+    profile.auth = const.AUTH_ALG_OPEN
+    profile.akm.append(const.AKM_TYPE_WPA2PSK)
+    profile.cipher = const.CIPHER_TYPE_CCMP
+    profile.key = password
+
+    iface.remove_all_network_profiles()  # Önceki profilleri kaldır
+    tmp_profile = iface.add_network_profile(profile)
+    iface.connect(tmp_profile)
+    time.sleep(3)  # Bağlantı süresi
+
+    if iface.status() == const.IFACE_CONNECTED:
+        print(f"{ssid} ağına başarıyla bağlanıldı.")
+        return True
+    else:
+        print(f"{ssid} ağına bağlanılamadı.")
+        return False
 
 # Kullanıcı bilgilerini JSON dosyasından oku
 def load_credentials():
@@ -23,19 +55,17 @@ def load_credentials():
         with open("user_info.json", "r") as file:
             return json.load(file)
 
-
+# ESOGU-WIFI ağına giriş
 def login_to_wifi():
     credentials = load_credentials()
     USERNAME = credentials["username"]
     PASSWORD = credentials["password"]
-    print("Kullanıcı bilgileriniz: ", USERNAME, PASSWORD)
 
     # ChromeDriver ayarları
     chrome_options = Options()
     chrome_options.add_argument("--headless")  # Tarayıcıyı arka planda çalıştırmak için (isteğe bağlı)
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-
 
     driver = webdriver.Chrome()
 
@@ -71,4 +101,9 @@ def login_to_wifi():
         driver.quit()
 
 if __name__ == "__main__":
-    login_to_wifi()
+    # Wi-Fi bağlantısını kontrol et ve bağlan
+    wifi_ssid = "ESOGU-WIFI"
+    wifi_password = "esogubim"  # Ağ şifresi
+    if connect_to_wifi(wifi_ssid, wifi_password):
+        # Giriş işlemini başlat
+        login_to_wifi()
